e303eb68659362c3f6d49e212494fcc9e5f19201
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2024-08-02 20:54:05 +0200

    Add get properties to EnvironmentInfo Class. Those deliver information about the runtime environment and host. Rename the class.

---
+++ EnvironmentLib/EnvironmentInfo.cs
@@ -0,0 +1,20 @@
+﻿// From Price, Mark J..Tools and Skills for .NET 8 
+// Chapter Containerizing your own .NET applications
+
+using System.Diagnostics;
+using System.Net; // To use Dns.
+using System.Runtime.InteropServices; // To use RuntimeInformation.
+
+namespace EnvironmentLib;
+
+public class EnvironmentInfo
+{
+    public string UserName { get; } = Environment.UserName;
+    public string HostName { get; } = Dns.GetHostName();
+    public string DotNet { get; } = RuntimeInformation.FrameworkDescription;
+    public string OS { get; } = RuntimeInformation.OSDescription;
+    public string Architecture { get; } = RuntimeInformation.OSArchitecture.ToString();
+    public int Processors { get; } = Environment.ProcessorCount;
+    public bool InContainer { get; } = Environment
+      .GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") is not null;
+}
